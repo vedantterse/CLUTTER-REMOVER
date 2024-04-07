8fda58ad7f9a122d1eed5f70d73d719bd3e0a9b3
Vedant terse <69134828+vedantterse@users.noreply.github.com>
2024-04-07 14:06:55 +0530

    updated version

---
+++ run1.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem Navigate to the directory where the Python script is located
+cd /d C:\Users\91976\PycharmProjects\pythonProject2
+
+rem Run the Python script
+python main1.py
+
+rem Pause to keep the command prompt window open after the script finishes
+pause
